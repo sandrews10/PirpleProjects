04ff1b100f63c26c255eb5e5a2f6ea7a2a3c1570
Steven Andrews <sandrewsjscc@gmail.com>
2018-11-17 22:16:00 -0600

    Put handler completed

--- assignment2/lib/handlers.js
+++ assignment2/lib/handlers.js
@@ -73,7 +73,7 @@ handlers._users.post = function(data, callback){
 							callback(200);
 						}else{
 							console.log(err);
-							callback(500, {'Error' : 'Could not create the new user'});
+							callback(500, {'Error' : 'Missing required field(s)'});
 						}
 					});
 
@@ -99,9 +99,51 @@ handlers._users.put = function(data, callback){
 
 	var firstName = typeof(data.payload.firstName) == 'string' && data.payload.firstName.trim().length > 0 ? data.payload.firstName.trim() : false;
 	var lastName = typeof(data.payload.lastName) == 'string' && data.payload.lastName.trim().length > 0 ? data.payload.lastName.trim() : false;
-	var phone = typeof(data.payload.phone) == 'string' && data.payload.phone.trim().length == 10 ? data.payload.phone.trim() : false;
 	var password = typeof(data.payload.password) == 'string' && data.payload.password.trim().length > 0 ? data.payload.password.trim() : false;
 	
+	if(phone){
+
+		if(firstName || lastName || password){
+			_data.read('users', phone, function(err, userData){
+				
+				if(!err && userData){
+					if(firstName){
+						userData.firstName = firstName;
+					}
+
+					if(lastName){
+						userData.lastName = lastName;
+					}
+
+					if(password){
+						userData.hashedPassword = helpers.hash(password);
+					}
+
+					_data.update('users', phone, userData, function(err){
+						if(!err){
+							callback(200);
+						}else{
+							console.log(err);
+							callback(500, {'Error' : 'Could not update the user'});
+						}
+					});
+
+				}else{
+					callback(400, {'Error' : 'The specified user does not exist'});
+				}
+			});
+
+		}else{
+			callback(400, {'Error' : 'Missing field(s) to update'});
+		}
+
+	}else{
+		callback(400, {'Error' : 'Missing required field(s)'});
+	}
+
+
+
+
 };
 
 handlers._users.delete = function(data, callback){
